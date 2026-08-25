df18cd0ce824a399de8398d84fbf6c5a4c50a7aa
anqck <khanhan8665074@gmail.com>
2026-08-25 16:20:59 +0700

    .

--- main.py
+++ main.py
@@ -307,7 +307,7 @@ def main(args):
     base_ds = get_coco_api_from_dataset(dataset_val)
 
     if args.frozen_weights is not None:
-        checkpoint = torch.load(args.frozen_weights, map_location="cpu")
+        checkpoint = torch.load(args.frozen_weights, map_location="cpu",  weights_only = False)
         model_without_ddp.detr.load_state_dict(
             clean_state_dict(checkpoint["model"]), strict=False
         )
@@ -321,7 +321,7 @@ def main(args):
                 args.resume, map_location="cpu", check_hash=True
             )
         else:
-            checkpoint = torch.load(args.resume, map_location="cpu")
+            checkpoint = torch.load(args.resume, map_location="cpu",  weights_only = False)
         model_without_ddp.load_state_dict(
             clean_state_dict(checkpoint["model"]), strict=False
         )
@@ -337,7 +337,7 @@ def main(args):
             args.start_epoch = checkpoint["epoch"] + 1
 
     if (not args.resume) and args.pretrain_model_path:
-        checkpoint = torch.load(args.pretrain_model_path, map_location="cpu")["model"]
+        checkpoint = torch.load(args.pretrain_model_path, map_location="cpu",  weights_only = False)["model"]
         from collections import OrderedDict
 
         _ignorekeywordlist = args.finetune_ignore if args.finetune_ignore else []
